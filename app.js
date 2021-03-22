194fc66e203eb572048eb20c9603f25be349073c
Eze Sandes <jezesandes@gmail.com>
2021-03-22 18:43:56 -0300

    V1.0 Quote Generator

--- app.js
+++ app.js
@@ -24,6 +24,46 @@ const quotes = [
       quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
       author: 'Martin Luther King Jr., A Testament of Hope: The Essential Writings and Speeches ',
    },
+   {
+      quote: "Those who cannot remember the past are condemned to repeat it.",
+      author: 'George Santayana',
+   },
+   {
+      quote: "If the brain were so simple we could understand it, we would be so simple we couldn't.",
+      author: 'Lyall Watson',
+   },
+   {
+      quote: "Any man who can drive safely while kissing a pretty girl is simply not giving the kiss the attention it deserves.",
+      author: 'Albert Einstein',
+   },
+   {
+      quote: "My kingdom for a horse!.",
+      author: 'Shakespeare (Richard III)',
+   },
+   {
+      quote: "I am become Death, the destroyer of worlds.",
+      author: 'J. Robert Oppenheimer(One of the fathers of the Atomic Bomb)',
+   },
+   {
+      quote: "Every particle of matter is attracted by or gravitates to every other particle of matter with a force inversely proportional to the squares of their distances.",
+      author: 'Isaac Newton',
+   },
+   {
+      quote: "There is only one good, knowledge, and one evil, ignorance.",
+      author: 'Socrates',
+   },
+   {
+      quote: "The Law is a fortress on a hill that armies cannot take or floods wash away.",
+      author: 'The Prophet Muhammed',
+   },
+   {
+      quote: "The voice of the people is the voice of God.",
+      author: 'Attributed to William of Malmesbury',
+   },
+   {
+      quote: "Whoever desires to found a state and give it laws, must start with assuming that all men are bad and ever ready to display their vicious nature, whenever they may find occasion for it.",
+      author: 'Niccolo Machiavelli',
+   },
 ];
 
 
